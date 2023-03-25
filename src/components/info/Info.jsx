f45c1f892f618c08cfe28d4d44c879e2073d0b9f
Obeysss <aybekkayumovv>
2023-03-26 01:25:19 +0500

    batafsil dizayn ozgardi

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -14,45 +14,92 @@ export default function Info() {
             });
     }, []);
     return (
-        <div className="container">
-            <div className="row g-0">
-                <div className="col-6">
-                    <img className="inf_img" src={Data.img} alt={Data.name} />
-                    <div className="imgs">
-                        <img className="img2" src={Data.img} alt={Data.name} />
-                        <img className="img2" src={Data.imgchet} alt="chet" />
+      // <div className="container">
+      <div className="tomon">
+        <div className="card2">
+          <img className="inf_img" src={Data.img} alt={Data.name} />
+          <div className="imgs">
+            <img className="img2" src={Data.img} alt={Data.name} />
+            <img className="img2" src={Data.imgchet} alt="chet" />
 
-                        <img className="img2" src={Data.imgold} alt="oldi" />
-
-                    </div>
-                </div>
-                <div className="col-6">
-                    <h1 className="txt1">
-                        <b>{Data.name}</b>
-                        {
-                            (Data.name == 'Iphone 12 pro' || Data.name == 'Iphone 12 pro selikonoviy' || Data.name == 'Iphone 13 pro') ?
-                                <>
-                                    <h4><b>Display:</b>{Data.display}</h4>
-                                    <h4><b>Chip:</b>{Data.chip}</h4>
-                                    <h4><b>Camera:</b>{Data.camera}</h4>
-                                    <h4><b>Zaryadi:</b>{Data.power}</h4>
-                                    <h4><b>OS:</b>{Data.OS}</h4>
-                                    <h4><b>Ogi'rligi:</b>{Data.gm}</h4>
-                                </>
-                                :
-                                <>
-
-                                    <h4><b>Typi:</b>{Data.type}</h4>
-                                    <h4><b>Consturctor:</b>{Data.constuctor1}</h4>
-                                    <h4><b>Decoratsiya:</b>{Data.oform}</h4>
-                                    <h4><b>Ulagich:</b>{Data.razem}</h4>
-                                    <h4><b>Qo'shimcha:</b>{Data.constuctor2}</h4>
-                                    <h4><b>Functiyasi</b>{Data.function}</h4>
-                                </>
-                        }
-                    </h1>
-                </div>
-            </div>
+            <img className="img2" src={Data.imgold} alt="oldi" />
+          </div>
         </div>
+        <div className="card3">
+          <h1 className="txt1">
+            <b>{Data.name}</b>
+            {Data.name == "Iphone 12 pro" ||
+            Data.name == "Iphone 12 pro selikonoviy" ||
+            Data.name == "Iphone 13 pro" ? (
+              <>
+                <h4>
+                  <b>Display:</b>
+                  {Data.display}
+                </h4>
+                <h4>
+                  <b>Chip:</b>
+                  {Data.chip}
+                </h4>
+                <h4>
+                  <b>Camera:</b>
+                  {Data.camera}
+                </h4>
+                <h4>
+                  <b>Zaryadi:</b>
+                  {Data.power}
+                </h4>
+                <h4>
+                  <b>OS:</b>
+                  {Data.OS}
+                </h4>
+                <h4>
+                  <b>Ogi'rligi:</b>
+                  {Data.gm}
+                </h4>
+              </>
+            ) : (
+              <>
+                <h4>
+                  <b>Typi:</b>
+                  {Data.type}
+                </h4>
+                <h4>
+                  <b>Consturctor:</b>
+                  {Data.constuctor1}
+                </h4>
+                <h4>
+                  <b>Decoratsiya:</b>
+                  {Data.oform}
+                </h4>
+                <h4>
+                  <b>Ulagich:</b>
+                  {Data.razem}
+                </h4>
+                <h4>
+                  <b>Qo'shimcha:</b>
+                  {Data.constuctor2}
+                </h4>
+                <h4>
+                  <b>Functiyasi</b>
+                  {Data.function}
+                </h4>
+              </>
+            )}
+            <h2 className="price">
+              <b>Narxi: {Data.price}$</b>
+            </h2>
+            <button className="korz">
+              Korzinka Qo'shish
+            </button>
+          </h1>
+          <h4 className="lorem">
+            <b>Malumot:</b> Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Facere obcaecati aliquid nisi maiores praesentium minima,
+            accusamus, placeat necessitatibus possimus maxime consectetur
+            voluptates dicta dolor enim ratione cupiditate odio rem saepe.
+          </h4>
+        </div>
+      </div>
+      // </div>
     );
 }
