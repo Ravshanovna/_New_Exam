114c94d2392054f03823037da2625093ff58d2a8
Obeysss <aybekkayumovv>
2023-03-26 16:37:05 +0500

    Ekzamen toliq tugadi

--- src/components/korzinka/Korzinka.jsx
+++ src/components/korzinka/Korzinka.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './korzinka.css'
-
+import {AiFillDelete} from 'react-icons/ai'
 const Korzinka = () => {
     const [data,setData] = useState([])
 
@@ -19,18 +19,34 @@ const Korzinka = () => {
               console.log(error);
             });
         }, []);
+
+        function Delete(params){
+                axios
+                  .put(
+                    `https://641d861d4366dd7def3fe5f8.mockapi.io/Qpick/${params}`,
+                    {
+                      status2:false,
+                    }
+                  )
+                  .then((res) => {
+                    console.log(res.data);
+                    window.location.reload();
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+        }
   return (
     <div className="omega">
       {data.length > 0 &&
         data.map((item, index) => {
           return (
             <div className="card1" key={index}>
-            
+              <AiFillDelete className="delete" onClick={()=>Delete(item.id)} />
               <img className="img" src={item.img} alt={item.name} />
               <h3 className="name">
                 <b>{item.name}</b>
               </h3>
-              
             </div>
           );
         })}
